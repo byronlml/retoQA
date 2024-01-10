49f2589c88cfa0bc3130236e298d8d40529ffcdf
@Bayron.Orozco <bayron.orozco@millicom.com>
2024-01-10 16:31:02 -0500

    test

--- src/Locators/login/Login.py
+++ src/Locators/login/Login.py
@@ -1,6 +1,6 @@
 class LocatorLogin:
     def __init__(self):
-
+        # test
         self.username_id = "user-name"
         self.password_id = "password"
         self.login_button_id = "login-button"
